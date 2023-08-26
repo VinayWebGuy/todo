74c05ade49b6a47c65b27fee4893070432eb762a
Vinay <vinaywebguy@gmail.com>
2023-08-26 11:31:21 +0530

    Add task form open and close functionality added

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.add-button button').click(function() {
+    $('.add-task-block').removeClass('hidden');
+})
+$('.close-button').click(function() {
+    $('.add-task-block').addClass('hidden');
+})
